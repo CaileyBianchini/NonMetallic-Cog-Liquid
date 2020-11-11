36d951bdf5277c86711d86c23b0a38517b68c61b
Cailey Bianchini <cailey.m.bianchini@gmail.com>
2020-11-11 15:36:54 -0600

    Changed evry Vector2 > Vector3 and changed everything to 3D

--- MathForGames3D/Actor.cs
+++ MathForGames3D/Actor.cs
@@ -1,10 +1,283 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Raylib_cs;
+using MathLibrary;
 
 namespace MathForGames3D
 {
     class Actor
     {
+        protected char _icon = ' ';
+
+        protected Vector3 _velocity;
+        protected Matrix4 _globalTransform = new Matrix4();
+        protected Matrix4 _localTransform = new Matrix4();
+        private Matrix4 _translation = new Matrix4();
+        private Matrix4 _scale = new Matrix4();
+        private Matrix4 _rotation = new Matrix4();
+
+        protected ConsoleColor _color;
+        protected Color _rayColor;
+        protected Actor _parent;
+        protected Actor[] _children = new Actor[0];
+        protected float _rotationAngle; //not needed for lookout
+        private float _collisionRadius;
+
+        public bool Started { get; private set; }
+
+        //SET 
+        public void SetTranslate(Vector3 position)
+        {
+            _translation = Matrix4.CreateTranslation(position);
+        }
+
+        public void SetTranslate(float x, float y, float z)
+        {
+            _translation.m13 = x;
+            _translation.m23 = y;
+            _translation.m33 = z;
+        }
+
+        //rotation
+        public void SetRotation(float radions)
+        {
+            _rotation.m11 = (float)Math.Cos(radions);
+            _rotation.m21 = -(float)Math.Sin(radions);
+            _rotation.m12 = (float)Math.Sin(radions);
+            _rotation.m22 = (float)Math.Cos(radions);
+        }
+
+        public void Rotate(float radians)
+        {
+            _rotation *= Matrix4.CreateRotation(radians);
+        }
+
+        public void localRotate(float angle)
+        {
+            Matrix4 m = new Matrix4((float)Math.Cos(angle), -(float)Math.Sin(angle), 0, (float)Math.Sin(angle), (float)Math.Cos(angle), 0, 0, 0, 1);
+
+
+            // temperary:\/ original:m._localTransform = m._localTransform * m;
+            _localTransform = _localTransform * m;
+        }
+
+        public void LookAt(Vector3 position)
+        {
+
+            Vector3 direction = (position - LocalPosition).Normalized;
+
+            float dotProd = Vector3.DotProduct(Forward, direction);
+            if (Math.Abs(dotProd) > 1)
+                return;
+            float angle = (float)Math.Acos(dotProd);
+
+            Vector3 perp = new Vector3(direction.Y, -direction.X, direction.Z); //<Don't know if Z should be - or not
+
+            float perpDot = Vector3.DotProduct(perp, Forward);
+
+            if (perpDot != 0)
+                angle *= -perpDot / Math.Abs(perpDot);
+
+            Rotate(angle);
+        }
+
+        public void SetScale(float x, float y, float z)
+        {
+            _scale = Matrix4.CreateScale(new Vector3(x, y, z));
+        }
+
+        private void UpdateTransform()
+        {
+            //combine translation, rotation and scale
+            _localTransform = (_translation * _rotation * _scale);
+        }
+
+        private void UpdateGlobalTransform()
+        {
+
+            if (_parent != null)
+                _globalTransform = _parent._globalTransform * _localTransform;
+            else
+                _globalTransform = Game.CurrentScene.World * _localTransform;
+        }
+
+        public Vector3 Forward
+        {
+            get
+            {
+                return new Vector3(_globalTransform.m11, _globalTransform.m21, _globalTransform.m23);
+            }
+        }
+
+        public Vector3 WorldPosition
+        {
+            get
+            {
+                return new Vector3(_globalTransform.m13, _globalTransform.m23, _globalTransform.m33);
+            }
+        }
+
+
+        public Vector3 LocalPosition
+        {
+            get { return new Vector3(_localTransform.m13, _localTransform.m23, _localTransform.m33); }
+            set
+            {
+                _translation.m13 = value.X;
+                _translation.m23 = value.Y;
+                _translation.m33 = value.Z;
+                //gotta fix this somehow
+                Vector3 lookPosition = LocalPosition + value.Normalized;
+                LookAt(lookPosition);
+
+            }
+        }
+
+        public Vector3 Velocity
+        {
+            get
+            {
+                return _velocity;
+            }
+            set
+            {
+                _velocity = value;
+            }
+        }
+
+        ///<summary>
+        ///this is the base class for all objects that will
+        /// be moverd or interacted with in the game
+        /// 
+        /// Create new matrices to transform the actors matrix. The user should be able
+        /// to translate, rotate, scale the actor.
+        /// </summary>
+
+        /// <param name="x">Position on the x axis</param>
+        /// <param name="y">Position on the y axis</param>
+        /// <param name="icon">The symbol that will appear when drawn</param>
+        /// <param name="color">The color of the symbol that will appear when drawn</param>
+        public Actor(float y, float x, float z, char icon = ' ')
+        {
+            _icon = icon;
+            _localTransform = new Matrix4();
+            LocalPosition = new Vector3(x, y, z);
+            _velocity = new Vector3();
+        }
+
+
+        /// <param name="x">Position on the x axis</param>
+        /// <param name="y">Position on the y axis</param>
+        /// <param name="rayColor">The color of the symbol that will appear when drawn to raylib</param>
+        /// <param name="icon">The symbol that will appear when drawn</param>
+        /// <param name="color">The color of the symbol that will appear when drawn to the console</param>
+        public Actor(float x, float y, Color rayColor, char icon = ' ', ConsoleColor color = ConsoleColor.White)
+            : this((char)x, y, icon)
+        {
+            _localTransform = new Matrix4();
+            _translation = new Matrix4();
+            _scale = new Matrix4();
+            _rotation = new Matrix4();
+            _rayColor = rayColor;
+        }
+
+        public void AddChild(Actor child)
+        {
+            Actor[] tempArray = new Actor[_children.Length + 1];
+
+            for (int i = 0; i < _children.Length; i++)
+            {
+                tempArray[i] = _children[i];
+            }
+
+            tempArray[_children.Length] = child;
+            _children = tempArray;
+            child._parent = this;
+        }
+
+        public bool RemoveChild(Actor child)
+        {
+            bool childRemoved = false;
+            if (child == null)
+                return false;
+
+            Actor[] tempArray = new Actor[_children.Length - 1];
+
+            int j = 0;
+            for (int i = 0; i < _children.Length; i++)
+            {
+                if (child != _children[i])
+                {
+                    tempArray[j] = _children[i];
+                    j++;
+                }
+                else
+                {
+                    childRemoved = true;
+                }
+            }
+
+            _children = tempArray;
+            child._parent = null;
+            return childRemoved;
+        }
+
+        /// <summary>
+        /// Updates the actors forward vector to be
+        /// the last direction it moved in
+        /// </summary>
+        private void UpdateFacing()
+        {
+            if (_velocity.Magnitude <= 0)
+                return;
+
+        }
+
+        public virtual void Start()
+        {
+            Started = true;
+        }
+
+
+        public virtual void Update(float deltaTime)
+        {
+            //Before the actor is moved, update the direction it's facing
+            UpdateFacing();
+
+            UpdateTransform();
+
+            //Increase position by the current velocity
+            LocalPosition += _velocity * deltaTime;
+
+            //Changes position by using Tranform
+            //_position *= _transform;
+
+            UpdateGlobalTransform();
+        }
+
+        public virtual void Draw()
+        {
+            //Draws the actor and a line indicating it facing to the raylib window.
+
+            //Changes the color of the console text to be this actors color
+            Console.ForegroundColor = _color;
+
+            //Only draws the actor on the console if it is within the bounds of the window
+            if (WorldPosition.X >= 0 && WorldPosition.X < Console.WindowWidth
+                && WorldPosition.Y >= 0 && WorldPosition.Y < Console.WindowHeight)
+            {
+                Console.SetCursorPosition((int)WorldPosition.X, (int)WorldPosition.Y);
+                Console.Write(_icon);
+            }
+
+            //Reset console text color to be default color
+            Console.ForegroundColor = Game.DefaultColor;
+        }
+
+        public virtual void End()
+        {
+            Started = false;
+        }
     }
 }
--- MathLibrary/Matrix4.cs
+++ MathLibrary/Matrix4.cs
@@ -6,5 +6,102 @@ namespace MathLibrary
 {
     class Matrix4
     {
+        public float m11, m12, m13, m21, m22, m23, m31, m32, m33;
+
+        public Matrix4()
+        {
+            m11 = 1; m12 = 0; m13 = 0;
+            m21 = 0; m22 = 1; m23 = 0;
+            m31 = 0; m32 = 0; m33 = 1;
+        }
+
+        public Matrix4(float m11, float m12, float m13,
+                       float m21, float m22, float m23,
+                       float m31, float m32, float m33)
+        {
+            this.m11 = m11; this.m12 = m12; this.m13 = m13;
+            this.m21 = m21; this.m22 = m22; this.m23 = m23;
+            this.m31 = m31; this.m32 = m32; this.m33 = m33;
+        }
+
+        public static Matrix4 operator +(Matrix4 lhs, Matrix4 rhs)
+        {
+            return new Matrix4
+            (
+                lhs.m11 + rhs.m11, lhs.m12 + rhs.m12, lhs.m13 + rhs.m13,
+                lhs.m21 + rhs.m21, lhs.m22 + rhs.m22, lhs.m23 + rhs.m23,
+                lhs.m31 + rhs.m31, lhs.m32 + rhs.m32, lhs.m33 + rhs.m33
+            );
+        }
+
+        public static Matrix4 operator -(Matrix4 lhs, Matrix4 rhs)
+        {
+            return new Matrix4
+            (
+                lhs.m11 - rhs.m11, lhs.m12 - rhs.m12, lhs.m13 - rhs.m13,
+                lhs.m21 - rhs.m21, lhs.m22 - rhs.m22, lhs.m23 - rhs.m23,
+                lhs.m31 - rhs.m31, lhs.m32 - rhs.m32, lhs.m33 - rhs.m33
+            );
+        }
+
+        public static Matrix4 operator *(Matrix4 lhs, Matrix4 rhs)
+        {
+            return new Matrix4
+            (
+                ///ROW 1///
+                //Row 1, Column1
+                lhs.m11 * rhs.m11 + lhs.m12 * rhs.m21 + lhs.m13 * rhs.m31,
+                //Row 1, Column2
+                lhs.m11 * rhs.m12 + lhs.m12 * rhs.m22 + lhs.m13 * rhs.m32,
+                //Row 1, Column3
+                lhs.m11 * rhs.m13 + lhs.m12 * rhs.m23 + lhs.m13 * rhs.m33,
+
+                ///ROW 2///
+                //Row 2, Column1
+                lhs.m21 * rhs.m11 + lhs.m22 * rhs.m21 + lhs.m23 * rhs.m31,
+                //Row 2, Column2
+                lhs.m21 * rhs.m12 + lhs.m22 * rhs.m22 + lhs.m23 * rhs.m32,
+                //Row 2, Column3
+                lhs.m21 * rhs.m13 + lhs.m22 * rhs.m23 + lhs.m23 * rhs.m33,
+
+                ///ROW 3///
+                //Row 3, Column1
+                lhs.m31 * rhs.m11 + lhs.m32 * rhs.m21 + lhs.m33 * rhs.m31,
+                //Row 3, Column2
+                lhs.m31 * rhs.m12 + lhs.m32 * rhs.m22 + lhs.m33 * rhs.m32,
+                //Row 3, Column3
+                lhs.m31 * rhs.m13 + lhs.m32 * rhs.m23 + lhs.m33 * rhs.m33
+            );
+        }
+
+        //CREATION
+
+        public static Matrix4 CreateRotation(float radians)
+        {
+            return new Matrix4(
+                (float)Math.Cos(radians), (float)Math.Sin(radians), 0,
+                -(float)Math.Sin(radians), (float)Math.Cos(radians), 0,
+                0, 0, 1);
+        }
+
+        public static Matrix4 CreateTranslation(Vector2 position)
+        {
+            return new Matrix4
+                (
+                    1, 0, position.X,
+                    0, 1, position.Y,
+                    0, 0, 1
+                );
+        }
+
+        public static Matrix4 CreateScale(Vector2 scale)
+        {
+            return new Matrix4
+                (
+                    scale.X, 0, 0,
+                    0, scale.Y, 0,
+                    0, 0, 1
+                );
+        }
     }
 }
